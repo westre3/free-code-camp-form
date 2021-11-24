eb9e11b8cc76e9880f9d629b4e0c246629ae0c3a
Trey West <robert.trey.west@gmail.com>
2021-11-24 13:02:37 -0500

    select and inputs of type text, range, radio fully implemented

--- functionality.js
+++ functionality.js
@@ -1,8 +1,24 @@
 const output = document.querySelector('output');
 const slider = document.querySelector('#slider');
+const sliderDots = document.querySelectorAll('.slider-dot-box');
+const outputMin = slider.getAttribute('min');
+const outputStep = slider.getAttribute('step');
+const outputMax = slider.getAttribute('max');
 
-output.innerText = slider.value;
+output.innerText = `${slider.value} doz.`;
 
 slider.addEventListener('input', () => {
-    output.innerText = slider.value;
+    output.innerText = `${slider.value} doz.`;
+    updateDots(slider.value);
 });
+
+function updateDots(newValue) {
+    sliderDots.forEach(dot => { dot.classList.remove('active'); });
+
+    // min and max are always transparent
+    if(newValue !== outputMin && newValue != output) {
+        // Figure out how many steps along the slider we are
+        const steps = (newValue - outputMin) / outputStep;
+        sliderDots[steps].classList.add('active');
+    }
+}
